60fd632504fad87b9eaf7697f80186b590433f88
bhavesh.gondane <bhavesh.gondane@neosoftmail.com>
2026-07-16 15:51:13 +0530

    Remove Duplicates

---
+++ DailyDSA/src/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+    }
+    public static int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int n : nums){
+            if(i<2||n!=nums[i-2]){
+                nums[i]=n;
+                i++;
+            }
+        }
+        return i;
+    }
+}
