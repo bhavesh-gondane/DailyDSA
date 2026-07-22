70e41ff39f3f7d05675a69dfbc3c616c11b140c2
bhavesh.gondane <bhavesh.gondane@neosoftmail.com>
2026-07-22 15:21:25 +0530

    Streams Practice

--- DailyDSA/src/java8/Streams.java
+++ DailyDSA/src/java8/Streams.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
@@ -44,7 +45,7 @@ public class Streams {
 
 //        Find the First Repeated Character in a String
         Character c1 = string.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(x->x,LinkedHashMap::new,Collectors.counting())).entrySet().stream().filter(x->x.getValue()>1).map(x->x.getKey()).findFirst().get();
-        System.out.println(c1);
+//        System.out.println(c1);
 
 //        Check if a String is a Palindrome
         String str = "abcba";
@@ -53,19 +54,30 @@ public class Streams {
         //OR
         String reversedString = new StringBuilder( str.chars().mapToObj(c->String.valueOf((char)c)).collect(Collectors.joining())).reverse().toString();
         boolean equals = str.equals(reversedString);
-        System.out.println(equals);
+//        System.out.println(equals);
 
 //        Check if Any String Matches a Condition
 //        Question: Check if any string in a list contains “API”.
         List<String> strings = Arrays.asList("Java", "Stream API", "Lambda");
         boolean api = strings.stream().anyMatch(x -> x.contains("API"));
-        System.out.println(api);
+//        System.out.println(api);
 
 //        10. Group Strings by Length
 //        Question: Group a list of strings based on their length.
         List<String> words = Arrays.asList("Java", "Stream", "API", "Code", "Fun");
         Map<Integer, List<String>> collect1 = words.stream().collect(Collectors.groupingBy(x->x.length()));
-        System.out.println(collect1);
+//        System.out.println(collect1);
 
+
+//        20. Partition Numbers into Even and Odd
+//        Question: Partition a list of numbers into even and odd.
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Map<Boolean, List<Integer>> collect2 = numbers.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+        System.out.println(collect2);
+
+//        21. Find Nth Largest Element in a List
+//        Question: Find the 3rd largest element in a list.
+        List<Integer> numbers1 = Arrays.asList(10, 20, 50, 40, 30);
+        Stream<Integer> skip = numbers1.stream().sorted().skip(2);
     }
 }
